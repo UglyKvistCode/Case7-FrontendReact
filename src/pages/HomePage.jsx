3606601a7d9d53b1c38286edc099cc49a210af58
Mattias Lager <mattias@addkolon.se>
2026-09-23 13:26:25 +0200

    Update HomePage.jsx

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -31,7 +31,7 @@ export default function HomePage() {
     <header>
       <img className="w-full h-[480px] object-cover" src="https://cdn.publisher-live.etc.nu/swp/uc3g8l/media/20211110161156_0d42f06dcc2c7c5cd828f4d6f516247712c0024b00a31fa3409456aabf33cfda.jpg"/>
     </header>
-    <main className="bg-gray-950 text-white h-screen p-12">
+    <main className="bg-gray-950 text-white p-12">
       <h1 className="text-3xl font-bold mb-6 text-center text-yellow-500">På bio just nu:</h1>
       <div className="flex gap-6 flex-wrap text-white p-8 justify-center w-2/3 m-auto">
       {movies.map(movie => (
